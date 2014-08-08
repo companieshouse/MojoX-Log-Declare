ad1f5941ced95f49ed58035317c1a86d1904ee5f
jenkins-coho <jenkins.coho@gmail.com>
2014-08-08 09:08:59 +0100

    0.32 9b6acd8

---
+++ lib/MojoX/Log/Declare.pm
@@ -0,0 +1,48 @@
+package MojoX::Log::Declare;
+
+#ABSTRACT: Integrate Log::Declare with Mojolicious
+
+use Mojo::Base -base;
+use Log::Declare;
+
+our $VERSION = '0.32';
+
+use strict;
+use warnings;
+
+{
+    no strict 'refs';
+    no warnings;
+
+    for my $level (@Log::Declare::level_priority) {
+        *{ __PACKAGE__ . "::$level" } = sub {
+            Log::Declare->log( $level, ['MOJO'], @_[1 .. $#_] );
+        };
+    }
+
+    *{ __PACKAGE__ . '::log' } = sub {
+        Log::Declare->log( 'info', ['MOJO'], @_[1 .. $#_] );
+    };
+
+    *{ 'MojoX::Log::Declare::history' } = *{ 'Mojo::Log::history' };
+}
+
+1;
+
+=head1 NAME
+
+MojoX::Log::Declare - Integrate Log::Declare with Mojolicious
+
+=head1 SYNOPSIS
+
+  use Mojo::Base 'Mojolicious';
+  use MojoX::Log::Declare;
+
+  sub startup {
+     my $self = shift;
+
+     $self->log( MojoX::Log::Declare->new() );
+     # ...
+  }
+
+=cut
